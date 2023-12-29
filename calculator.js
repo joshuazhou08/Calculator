ba44974c565019e8e444d4e234e923f88480064a
joshuazhou08 <joshuazhou0808@gmail.com>
2023-12-29 14:46:28 -0800

    Create Calculation Function
    The calculation() function takes three args and calls one of the add,
    subtract, multiply, or divide function depending on the 'operator' arg.
    The three variables storing the operands and the operator have also
    been initialized.

--- calculator.js
+++ calculator.js
@@ -25,3 +25,24 @@ const divide = function(a,b){
     }
     return numA / numB
 }
+
+//Initialize variables containing the two numbers to be operated on and the operator
+let num1
+let num2
+let operation
+
+//Function that calls one of the add, subtract, multiple, or divide functions given three args: two #'s and a operation
+const calculation = function(a, b, operator) {
+    if (operator == 'add'){
+        return add(a, b)
+    }
+    else if (operator == 'subtract'){
+        return subtract(a, b)
+    }
+    else if (operator == 'multiply'){
+        return multiply(a, b)
+    }
+    else if (operator == 'divide'){
+        return divide(a, b)
+    }
+}
